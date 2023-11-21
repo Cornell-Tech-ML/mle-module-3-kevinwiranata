dc11c2da7f046f6505727ba2b8d1e9222709dc8b
kevinwiranata <kevin.a.wiranata@gmail.com>
2023-11-20 21:09:33 -0500

    graph generator

---
+++ minitorch/generate_graphs.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+import minitorch
+import minitorch.fast_ops
+import minitorch
+from minitorch import TensorBackend
+from typing import Dict
+# Define problem sizes
+problem_sizes = [10, 100, 250, 500, 1000, 2500, 5000]
+
+# Initialize list to store times
+times = []
+
+# For each problem size
+FastTensorBackend = minitorch.TensorBackend(minitorch.FastOps)
+shared: Dict[str, TensorBackend] = {"fast": FastTensorBackend}
+for N in problem_sizes:
+    # Initialize NxN tensor
+    tensor = np.random.rand(N, N)
+    # Assuming `tensor` is a numpy ndarray
+    #tensor = minitorch.Tensor(tensor, backend=shared["fast"])
+
+    # Start timer
+    start_time = time.time()
+
+    # Perform tensor matrix multiplication
+    tensor @ tensor
+
+    # Stop timer and record time
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    times.append(elapsed_time)
+
+# Plot problem sizes against times
+plt.plot(problem_sizes, times)
+plt.xlabel('Problem Size (N)')
+plt.ylabel('Time (seconds)')
+plt.title('Performance of _tensor_matrix_multiply')
+plt.grid()
+plt.legend()
+plt.show()
+print(problem_sizes, times)
